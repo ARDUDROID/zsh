73f45a4d58bbcf08f593e20fea148d5a0103dcc0
Adrian <57409635+ARDUDROID@users.noreply.github.com>
2022-01-02 20:25:48 +0100

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -6,14 +6,6 @@ sudo chsh -s /bin/zsh
 cp -i .bashrc{,.bak}
 cp -i /etc/skel/.bashrc ~/
 source ~/.bashrc
-echo 'root'
-sudo su 
-[ -e ~/.zshrc ] && cp -i .zshrc{,.bak}
-cp -i /etc/skel/.zshrc ~/
-sudo chsh -s /bin/zsh
-cp -i .bashrc{,.bak}
-cp -i /etc/skel/.bashrc ~/
-source ~/.bashrc
 echo 'reboot:)'
    reboot
 fi
