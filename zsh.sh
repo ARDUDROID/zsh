36560d36c6d32c5e3ce13950525cf28c520cd34e
Adrian <57409635+ARDUDROID@users.noreply.github.com>
2022-01-02 20:17:59 +0100

    Add files via upload

---
+++ zsh.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+echo 'normal'
+[ -e ~/.zshrc ] && cp -i .zshrc{,.bak}
+cp -i /etc/skel/.zshrc ~/
+sudo chsh -s /bin/zsh
+cp -i .bashrc{,.bak}
+cp -i /etc/skel/.bashrc ~/
+source ~/.bashrc
+echo 'root'
+sudo su 
+[ -e ~/.zshrc ] && cp -i .zshrc{,.bak}
+cp -i /etc/skel/.zshrc ~/
+sudo chsh -s /bin/zsh
+cp -i .bashrc{,.bak}
+cp -i /etc/skel/.bashrc ~/
+source ~/.bashrc
+echo 'reboot:)'
+   reboot
+fi
